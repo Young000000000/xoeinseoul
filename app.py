1018b0b671d00dd6720b744ac3592a4a1b92dc36
Young000000000 <young@mineis.io>
2024-11-08 17:15:16 +0900

    version1-16

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout="wide")
+
 # Load data from CSV file
 def load_data(filename="xoeinseoul.csv"):
     data = pd.read_csv(filename)
